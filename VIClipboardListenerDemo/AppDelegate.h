b8da5e3c0ead258b73b0f20727e3c8ab653889c2
vito <zhangwei.noair@gmail.com>
2013-05-17 15:40:31 +0800

    Initial Commit

---
+++ VIClipboardListenerDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VIClipboardListenerDemo
+//
+//  Created by Vito on 13-5-17.
+//  Copyright (c) 2013年 vito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
