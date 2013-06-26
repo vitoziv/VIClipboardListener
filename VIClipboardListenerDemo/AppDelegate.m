d86195bf66c8ea2b514b28b896148e5caa4f601b
zolibra <zolibra@gmail.com>
2013-06-26 13:26:36 +0800

    Fix 2 issue: 1, Demo not work; 2, Should stop the timer loop when comes back to the frond

--- VIClipboardListenerDemo/AppDelegate.m
+++ VIClipboardListenerDemo/AppDelegate.m
@@ -29,21 +29,31 @@
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    [[VIClipboardListener share] setDelegate:self];
+    [[VIClipboardListener share] startListener];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        NSLog(@"Got applicationWillEnterForeground");
+
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    NSLog(@"Got applicationDidBecomeActive ");
+    [[VIClipboardListener share] stopListener];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (void)pasteboardChanged:(UIPasteboard *)generatePasteboard{
+    NSLog(@"Got pasteboard chagne");
 }
 
 @end
